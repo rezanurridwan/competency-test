828670b146a3f2da521fd186d8ebafec5aeb4654
Reza  Nur Ridwan <jeypecker01@gmail.com>
2021-11-10 10:39:34 +0700

    menambahkan port

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ const http = require ('http')
 const path = require ('path')
 const express = require ('express')
 const {dirname} = require('path')
+
+const app = express()
+app.use(express.json())
+
+app.set ('view engine', 'hbs')
+
+
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+console.debug('Server listening on port ' + port);
+
+hbs.handlebars.registerHelper('isAuth', function (value) {
+    if (value == true) {
+      return false;
+    } else {
+      return true;
+    }
+  });
