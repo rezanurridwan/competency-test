130e4994fb01ec08d08a63dba13c612bd5b6f477
Reza  Nur Ridwan <jeypecker01@gmail.com>
2021-11-10 13:19:03 +0700

    menambahkan header, footer dan nav

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const http = require ('http')
 const path = require ('path')
 const express = require ('express')
-const {dirname} = require('path')
+const { dirname } = require('path')
+const hbs = require ('hbs')
+
 
 const app = express()
 app.use(express.json())
 
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set ('view engine', 'hbs')
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+app.get ('/', function (request, response){
+    const title = "Task Collections"
+
+    response.render('index',{
+        title : title
+    })
+})
 
 
 const server = http.createServer(app);
 const port = 3000;
 server.listen(port);
 console.debug('Server listening on port ' + port);
-
-hbs.handlebars.registerHelper('isAuth', function (value) {
-    if (value == true) {
-      return false;
-    } else {
-      return true;
-    }
-  });
